59bb66678da38ef9317d12a0010381822917b0a6
sam <sburba@mail.csuchico.edu>
2017-12-02 20:13:22 -0800

    testing creating the table

--- viterbi.py
+++ viterbi.py
@@ -12,9 +12,7 @@ class Viterbi:
         self.table = np.zeros((self.num_states, self.num_obs))
 
     def get_obs_num(self, obs):
-        for i, ob in enumerate(self.possible_obs):
-            if obs is ob:
-                return i
+        return self.possible_obs.index(obs)
 
     def run(self):
         path = []
@@ -22,14 +20,47 @@ class Viterbi:
         for st in range(self.num_states):
             self.table[st][0] = self.initial[st]
         #find the value for each state at each time
-        for ob in range(self.num_obs):
-            prob = []
-            for st1 in range(self.num_states):
-                for st2 in range(self.num_states):
-                    p = self.table[ob][st1] * self.trans[ob][st2] * self.emiss[0][self.get_obs_num(self.obs[ob])]
-                    prob.append(p)
-                print(prob)
+        prob = []
+        #B
+        print(self.table[0][0])
+        print(self.trans[0][0])
+        print(self.emiss[0][1])
+        print(self.table[0][0])
+        print(self.trans[0][1])
+        print(self.emiss[0][1])
+        print(self.table[0][0])
+        print(self.trans[0][2])
+        print(self.emiss[0][1])
         print(prob)
+        #L
+        print(self.table[1][0])
+        print(self.trans[1][0])
+        print(self.emiss[1][1])
+        print(self.table[1][0])
+        print(self.trans[1][1])
+        print(self.emiss[1][1])
+        print(self.table[1][0])
+        print(self.trans[1][2])
+        print(self.emiss[1][1])
+        print(prob)
+        print("in loop")
+        for ob in range(self.num_obs):
+            observed = self.possible_obs.index(obs[ob])
+            for i in range(self.num_states):
+                for j in range(self.num_states):
+                    #p = self.table[i][0] * self.trans[i][j] * self.emiss[i][1]
+                    print(self.table[i][ob])
+                    print(self.trans[i][j])
+                    print(self.emiss[i][observed])
+
+        # for ob in range(self.num_obs - 1):
+        #     prob = []
+        #     for st1 in range(self.num_states):
+        #         for st2 in range(self.num_states):
+        #             p = self.table[ob][st1] * self.trans[ob][st2] * self.emiss[0][self.get_obs_num(self.obs[ob])]
+        #             prob.append(p)
+        #             print(prob)
+        # print(prob)
 
     def print_table(self):
         print(self.table)
